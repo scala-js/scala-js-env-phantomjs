f05d49e17eb18bb9d4ed6eb96e62c982e14d9bfb
Tobias Schlatter <tobias@meisch.ch>
2019-11-23 07:52:26 +0100

    Remove filter for `largeMessageTest`
    The test was fixed in 1.0.0-M7 already.

--- phantomjs-env/src/test/scala/org/scalajs/jsenv/phantomjs/PhantomJSSuite.scala
+++ phantomjs-env/src/test/scala/org/scalajs/jsenv/phantomjs/PhantomJSSuite.scala
@@ -4,41 +4,5 @@ import org.scalajs.jsenv.test._
 
 import org.junit.runner.RunWith
 
-import scala.collection.JavaConverters._
-
-import org.junit.runner.Runner
-import org.junit.runners.Suite
-import org.junit.runner.manipulation.Filter
-import org.junit.runner.Description
-
-@RunWith(classOf[PhantomJSSuiteRunner])
-class PhantomJSSuite private (private[phantomjs] val _config: JSEnvSuiteConfig)
-    extends JSEnvSuite(_config) {
-
-  def this() = this(JSEnvSuiteConfig(new PhantomJSEnv))
-}
-
-/** A runner that wraps `JSEnvSuiteRunner` and ignores `largeMessageTest`.
- *
- *  This works around [[https://github.com/scala-js/scala-js/issues/3476]]:
- *  `largeMessageTest` sends messages that are not valid UTF-16 strings, which
- *  is invalid.
- */
-final class PhantomJSSuiteRunner private (
-    root: Class[_], base: JSEnvSuiteRunner)
-    extends Suite(root, List[Runner](base).asJava) {
-
-  /** Constructor for reflective instantiation via `@RunWith`. */
-  def this(suite: Class[_ <: PhantomJSSuite]) =
-    this(suite, new JSEnvSuiteRunner(suite.newInstance()._config))
-
-  // Apply a filter that ignores all tests whose method name is `largeMessageTest`
-  base.filter(new Filter {
-    def describe(): String = "Ignore largeMessageTest"
-
-    def shouldRun(description: Description): Boolean = {
-      description.getMethodName == null ||
-      !description.getMethodName.startsWith("largeMessageTest")
-    }
-  })
-}
+@RunWith(classOf[JSEnvSuiteRunner])
+class PhantomJSSuite extends JSEnvSuite(JSEnvSuiteConfig(new PhantomJSEnv))
